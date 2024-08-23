8c49d5157a070799face6a1850dd273084bb7171
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-08-23 23:39:24 +0530

    Update c.py

--- c.py
+++ c.py
@@ -15,7 +15,7 @@ OWNER_ID = 7427691214  # Owner's Telegram ID
 bot = telebot.TeleBot(TOKEN)
 
 # Define the API endpoint and static parameters
-url = "https://mrdaxx.com/daxxapi/chk/chk.php"
+url = "https://mrdaxx.com/api.php"
 
 # Event to control the stopping of the card check process
 stop_event = Event()
